7b21dc6fcf18bae530279f90db8483a67f0863e9
safdari7 <sam.dreamkiller2018@gmail.com>
2022-07-14 11:12:31 +0430

    4th Chapter Done

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,14 +1,30 @@
+import * as auth from 'auth-provider'
 const apiURL = process.env.REACT_APP_API_URL
 
-function client(endpoint, customConfig = {}) {
+function client(
+  endpoint,
+  {token, headers: customHeaders, data, ...customConfig} = {},
+) {
   const config = {
-    method: 'GET',
+    method: data ? 'POST' : 'GET',
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+      'Content-Type': data ? 'application/json' : undefined,
+      ...customHeaders,
+    },
+    body: data ? JSON.stringify(data) : undefined,
     ...customConfig,
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    if (response.status === 401) {
+      await auth.logout()
+      window.location.assign(window.location)
+      return Promise.reject({message: 'please re-authenticate'})
+    }
     const data = await response.json()
     if (response.ok) {
+      console.log(response)
       return data
     } else {
       return Promise.reject(data)
